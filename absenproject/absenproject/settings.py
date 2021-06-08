96123c583fc184d22027ba1010e154a7d346f74a
Rachman <rmrachmanfauzan@gmail.com>
2021-06-08 07:59:49 +0700

    deploy

--- absenproject/absenproject/settings.py
+++ absenproject/absenproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wwayou8b8%dt4om0t#tldnd$xwb9-ied97=mih)rq!78jzn$!c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','testabsensi.herokuapp.com']
 
 
 # Application definition
